9425438a99577a0c999917373f4074afb6d3fe53
raquel <raquelanaroman@gmail.com>
2021-05-03 12:26:12 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000);
+function add(a, b){
+  return a + b ;
+}
+
+var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000);
+function subtract(a, b){
+  return a - b;
+}
+
+var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000);
+function multiply(a, b){
+  return a * b;
+}
+
+var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000);
+function divide(a, b){
+  return a / b;
+}
+
+var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000);
+function increment(a){
+  return a += 1;
+}
+
+var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000);
+function decrement(a){
+  return a -= 1;
+}
+
+var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000);
+function makeInt(a){
+  return parseInt(a, 10);
+}
+
+var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000);
+function preserveDecimal(a){
+  return parseFloat(a);
+}
